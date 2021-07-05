ac449c90d5125782b940528a1b2190f36f534871
Able <erugalev@objectstyle.com>
2021-07-05 14:32:19 +0300

    + Customer Tipzz module + Success Payment page + refactored structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,4 +19,21 @@ jQuery(document).ready(function() {
     $(".upload-button").on('click', function() {
        $(".file-upload").click();
     });
+
+    // Btn back
+    $('.btn#back').on('click', function(e){
+        e.preventDefault();
+        window.history.back();
+    });
+
+    // Btn Tipzz
+    $('#tipzz-option-1, #tipzz-option-2, #tipzz-option-3, #tipzz-option-4').on('click', function(e) {
+        e.preventDefault();
+        $('#tipzz-summ-val').val($(this).text());
+    });
+
+    $('#tipzz-summ-val-clear').on('click', function(e){
+        e.preventDefault();
+        $('#tipzz-summ-val').val('');
+    });
 });
